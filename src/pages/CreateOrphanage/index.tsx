be0b9f55440d8e3546b8b45b6fc0b3a815fabdce
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-10-14 19:51:04 -0300

    :sparkles: feat: create a orphanage

--- src/pages/CreateOrphanage/index.tsx
+++ src/pages/CreateOrphanage/index.tsx
@@ -1,101 +1,198 @@
-import React from 'react';
+import React, { useState, FormEvent, ChangeEvent } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Map, Marker, TileLayer } from 'react-leaflet';
-import L from 'leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 import { FiPlus } from 'react-icons/fi';
 
 import Sidebar from '../../components/Sidebar';
-import mapMarkerImg from '../../assets/map-marker.svg';
+import happyMapIcon from '../../utils/happyMapIcon';
+import api from '../../services/api';
 
 import './styles.css';
 
-const happyMapIcon = L.icon({
-  iconUrl: mapMarkerImg,
+const CreateOrphanage: React.FC = () => {
+  const history = useHistory();
 
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60],
-});
+  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
 
-const CreateOrphanage: React.FC = () => (
-  <div id="page-create-orphanage">
-    <Sidebar />
+  const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekends, setOpenOnWeekends] = useState(true);
+  const [images, setImages] = useState<File[]>([]);
+  const [prevewImages, setPrevewImages] = useState<string[]>([]);
 
-    <main>
-      <form className="create-orphanage-form">
-        <fieldset>
-          <legend>Dados</legend>
+  function handleMapClik(event: LeafletMouseEvent) {
+    const { lat, lng } = event.latlng;
 
-          <Map
-            center={[-27.2092052, -49.6401092]}
-            style={{ width: '100%', height: 280 }}
-            zoom={15}
-          >
-            <TileLayer
-              url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-            />
+    setPosition({
+      latitude: lat,
+      longitude: lng,
+    });
+  }
 
-            <Marker
-              interactive={false}
-              icon={happyMapIcon}
-              position={[-27.2092052, -49.6401092]}
-            />
-          </Map>
+  function handleSelectImage(event: ChangeEvent<HTMLInputElement>) {
+    if (!event.target.files) return;
 
-          <div className="input-block">
-            <label htmlFor="name">Nome</label>
-            <input id="name" />
-          </div>
+    const selectedImage = Array.from(event.target.files);
 
-          <div className="input-block">
-            <label htmlFor="about">
-              Sobre <span>Máximo de 300 caracteres</span>
-            </label>
-            <textarea id="name" maxLength={300} />
-          </div>
+    setImages(selectedImage);
 
-          <div className="input-block">
-            <label htmlFor="images">Fotos</label>
+    const selectedImagePreview = selectedImage.map(image =>
+      URL.createObjectURL(image));
 
-            <div className="uploaded-image" />
+    setPrevewImages(selectedImagePreview);
+  }
 
-            <button className="new-image" type="button">
-              <FiPlus size={24} color="#15b6d6" />
-            </button>
-          </div>
-        </fieldset>
+  async function handleSubmit(event: FormEvent) {
+    event.preventDefault();
 
-        <fieldset>
-          <legend>Visitação</legend>
+    const { latitude, longitude } = position;
 
-          <div className="input-block">
-            <label htmlFor="instructions">Instruções</label>
-            <textarea id="instructions" />
-          </div>
+    const data = new FormData();
 
-          <div className="input-block">
-            <label htmlFor="opening_hours">Nome</label>
-            <input id="opening_hours" />
-          </div>
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
 
-          <div className="input-block">
-            <label htmlFor="open_on_weekends">Atende fim de semana</label>
+    images.forEach(image => {
+      data.append('images', image);
+    });
 
-            <div className="button-select">
-              <button type="button" className="active">
-                Sim
-              </button>
-              <button type="button">Não</button>
+    await api.post('orphanages', data);
+
+    alert('Cadastro realizado com sucesso!');
+
+    history.push('/app');
+  }
+
+  return (
+    <div id="page-create-orphanage">
+      <Sidebar />
+
+      <main>
+        <form onSubmit={handleSubmit} className="create-orphanage-form">
+          <fieldset>
+            <legend>Dados</legend>
+
+            <Map
+              center={[-22.701093, -43.531849]}
+              style={{ width: '100%', height: 280 }}
+              zoom={15}
+              onclick={handleMapClik}
+            >
+              <TileLayer
+                url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+              />
+
+              {position.latitude !== 0 && (
+                <Marker
+                  interactive={false}
+                  icon={happyMapIcon}
+                  position={[position.latitude, position.longitude]}
+                />
+              )}
+            </Map>
+
+            <div className="input-block">
+              <label htmlFor="name">Nome</label>
+              <input
+                id="name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
             </div>
-          </div>
-        </fieldset>
 
-        <button className="confirm-button" type="submit">
-          Confirmar
-        </button>
-      </form>
-    </main>
-  </div>
-);
+            <div className="input-block">
+              <label htmlFor="about">
+                Sobre <span>Máximo de 300 caracteres</span>
+              </label>
+              <textarea
+                id="name"
+                maxLength={300}
+                value={about}
+                onChange={e => setAbout(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="images">Fotos</label>
+
+              <div className="images-container">
+                {prevewImages.map(image => (
+                  <img key={image} src={image} alt={name} />
+                ))}
+
+                <label htmlFor="image[]" className="new-image">
+                  <FiPlus size={24} color="#15b6d6" />
+                </label>
+              </div>
+              <input
+                multiple
+                onChange={handleSelectImage}
+                type="file"
+                id="image[]"
+              />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <div className="input-block">
+              <label htmlFor="instructions">Instruções</label>
+              <textarea
+                id="instructions"
+                value={instructions}
+                onChange={e => setInstructions(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="opening_hours">Horário de funcionamento</label>
+              <input
+                id="opening_hours"
+                value={opening_hours}
+                onChange={e => setOpeningHours(e.target.value)}
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="open_on_weekends">Atende fim de semana</label>
+
+              <div className="button-select">
+                <button
+                  type="button"
+                  className={open_on_weekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(true)}
+                >
+                  Sim
+                </button>
+
+                <button
+                  type="button"
+                  className={!open_on_weekends ? 'active' : ''}
+                  onClick={() => setOpenOnWeekends(false)}
+                >
+                  Não
+                </button>
+              </div>
+            </div>
+          </fieldset>
+
+          <button className="confirm-button" type="submit">
+            Confirmar
+          </button>
+        </form>
+      </main>
+    </div>
+  );
+};
 
 export default CreateOrphanage;
 
